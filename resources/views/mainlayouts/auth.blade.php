f5c709b965a71c49c397997f9cb1b3389d7b520e
Maryadi <maryadi@astrnt.co>
2017-09-05 15:53:00 +0700

    add less

--- resources/views/mainlayouts/auth.blade.php
+++ resources/views/mainlayouts/auth.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="_token" content="{{ csrf_token() }}" />
     <title>@yield('title')</title>
-    <link rel="stylesheet" type="text/css" href="{{ mix('dist/css/admin.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ mix('hyosoka_dist/css/admin.css') }}">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -18,7 +18,7 @@
 <div class="wrapper">
     @yield('content')
 </div>
-<script src="{{ mix('dist/js/admin.js') }}" type="text/javascript"></script>
+<script src="{{ mix('hyosoka_dist/js/admin.js') }}" type="text/javascript"></script>
 </body>
 </html>
 
